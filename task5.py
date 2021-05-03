3fc1fc458173adb4bb3f0d3a8749d058ae197e15
andreevladimir406 <andreevladimir406@yandex.ru>
2021-05-03 18:20:03 +0300

    middle of task5

--- task5.py
+++ task5.py
@@ -28,4 +28,60 @@ stuffdict = {'couch_s':(300,75),
              'chest':(150,25),
              'heater':(100,25)
             }
+def get_area_and_value(stuffdict):
+    area = [stuffdict[item][0] for item in stuffdict]
+    value = [stuffdict[item][1] for item in stuffdict]        
+    return area, value
+def get_memtable(stuffdict, A=2000):
+      area, value = get_area_and_value(stuffdict)
+      n = len(value) # находим размеры таблицы
+      
+      # создаём таблицу из нулевых значений
+      V = [[0 for a in range(A+1)] for i in range(n+1)]
 
+      for i in range(n+1):
+            for a in range(A+1):
+                  # базовый случай
+                  if i == 0 or a == 0:
+                        V[i][a] = 0
+
+                  # если площадь предмета меньше площади столбца,
+                  # максимизируем значение суммарной ценности
+                  elif area[i-1] <= a:
+                        V[i][a] = max(value[i-1] + V[i-1][a-area[i-1]], V[i-1][a])
+
+                  # если площадь предмета больше площади столбца,
+                  # забираем значение ячейки из предыдущей строки
+                  else:
+                        V[i][a] = V[i-1][a]       
+      return V, area, value
+
+def get_selected_items_list(stuffdict, A=2000):
+      V, area, value = get_memtable(stuffdict)
+      n = len(value)
+      res = V[n][A]      # начинаем с последнего элемента таблицы
+      a = A              # начальная площадь - максимальная
+      items_list = []    # список площадей и ценностей
+    
+      for i in range(n, 0, -1):  # идём в обратном порядке
+            if res <= 0:  # условие прерывания - собрали "рюкзак" 
+                  break
+            if res == V[i-1][a]:  # ничего не делаем, двигаемся дальше
+                  continue
+            else:
+                  # "забираем" предмет
+                  items_list.append((area[i-1], value[i-1]))
+                  res -= value[i-1]   # отнимаем значение ценности от общей
+                  a -= area[i-1]  # отнимаем площадь от общей
+            
+      selected_stuff = []
+
+      # находим ключи исходного словаря - названия предметов
+      for search in items_list:
+            for key, value in stuffdict.items():
+                  if value == search:
+                        selected_stuff.append(key)
+            
+      return selected_stuff
+
+stuff = get_selected_items_list(stuffdict)
